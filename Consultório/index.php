b0650880a4091ece3c13475474ce5099291a2a76
guilhermebregon <110949870+guilhermebregon@users.noreply.github.com>
2023-10-10 21:48:56 -0300

    Add files via upload

---
+++ Consultório/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Teste Bootstrap com PHP</title>
+<link rel="stylesheet" href="css/bootstrap.min.css"></link> 
+<link rel="stylesheet" href="globals.css" />
+<link rel="stylesheet" href="style.css"></link>
+</head>
+<header>    
+    <div class="box"><img class="vector" src="img/vector.svg">
+
+    </div>
+    
+</header>
+<body>
+
+<h1>funcionou sem php</h1>
+
+<?php
+
+echo "<h1>funcionou com php</h1>";
+
+?>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="box"><img class="vector" src="img/vector.svg" /></div>
+  </body>
+</html>
